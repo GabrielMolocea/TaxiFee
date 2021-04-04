fe62c028816ed602ba081ca4e122fb21db354acd
GabrielMolocea <gabrielgabriel1235@gmail.com>
2021-04-04 15:38:27 +0300

    Inital commit

---
+++ app/src/main/java/com/gabriel/taxifee/_common.java
@@ -0,0 +1,11 @@
+package com.gabriel.taxifee;
+
+import com.gabriel.taxifee.model.DriverInfoModel;
+
+public class _common {
+    public static final String DRIVER_INFO_REFERENCE = "DriverInfo";
+    public static final String DRIVER_LOCATION_REFERENCES = "DriverLocation";
+
+    public static DriverInfoModel currentUser;
+
+}
